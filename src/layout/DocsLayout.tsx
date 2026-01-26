279a0dc694a7084c37ea4f572dffece1d5b89d39
Adrian Lona <104098971+AdrianLonaFragoso@users.noreply.github.com>
2026-01-26 08:51:03 -0600

    fix: padding lower size screens

--- src/layout/DocsLayout.tsx
+++ src/layout/DocsLayout.tsx
@@ -8,28 +8,28 @@ import Footer from './Footer';
 import ModalFeedback from '../components/ModalFeedback';
 
 const DocsLayout = () => {
-	const [showModal, setShowModal] = useState('hidden');
+  const [showModal, setShowModal] = useState('hidden');
 
-	const toggleModal = () => {
-		setShowModal((prev) => (prev === 'hidden' ? '' : 'hidden'));
-	};
-	return (
-		<>
-			<Navbar />
-			<main className="flex flex-row bg-off_white h-screen w-screen overflow-hidden">
-				<SidebarV2 />
-				<div className="flex flex-col overflow-y-scroll overflow-x-visible w-full">
-					<div className="flex px-8 pt-5 md:px-15 xl:px-20 flex-1">
-						<Outlet context={{ toggleModal }} />
-					</div>
-					<Footer toggleModal={toggleModal} />
-				</div>
-			</main>
-			<Toast />
-			<Dialog />
-			<ModalFeedback showModal={showModal} toggleModal={toggleModal} />
-		</>
-	);
+  const toggleModal = () => {
+    setShowModal((prev) => (prev === 'hidden' ? '' : 'hidden'));
+  };
+  return (
+    <>
+      <Navbar />
+      <main className="flex flex-row bg-off_white h-screen w-screen overflow-hidden">
+        <SidebarV2 />
+        <div className="flex flex-col overflow-y-scroll overflow-x-visible w-full">
+          <div className="flex px-8 pt-5 md:px-15 2xl:px-20 flex-1">
+            <Outlet context={{ toggleModal }} />
+          </div>
+          <Footer toggleModal={toggleModal} />
+        </div>
+      </main>
+      <Toast />
+      <Dialog />
+      <ModalFeedback showModal={showModal} toggleModal={toggleModal} />
+    </>
+  );
 };
 
 export default DocsLayout;
